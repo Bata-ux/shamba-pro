f482ad12cbf04e9579741a09b97f378aea437523
Bata-ux <ibrahimbatambuze@gmail.com>
2025-06-15 15:22:38 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import Head from "next/head";
-import "../styles/globals.css";
 import { Button } from "../components/ui/button";
 import { Input } from "../components/ui/input";
 import { 
